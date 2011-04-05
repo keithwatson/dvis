afd4a29c1f27114829bcdde8c0a309e866562e96
keithmatthewwatson <keithmatthewwatson@gmail.com>
2011-04-05 18:27:02 +0100

    Removed comments

--- src/com/datavisualisation/fileupload/FileUploadServlet.java
+++ src/com/datavisualisation/fileupload/FileUploadServlet.java
@@ -20,7 +20,6 @@ public class FileUploadServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 4507877741171768330L;
 
-	/* tits */
 
 	public FileUploadServlet() {
 		super();
--- src/com/datavisualisation/parsers/CsvParser.java
+++ src/com/datavisualisation/parsers/CsvParser.java
@@ -22,8 +22,6 @@ public class CsvParser {
 	}
 
 
-	/* Arse feck */
-	
 	public static void debug(List<String[]> rows) {
 
 		for (String[] row : rows) {
